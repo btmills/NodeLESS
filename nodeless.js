6995e9dd737b0c435037194a65bf68d444395631
btmills <mills.brandont@gmail.com>
2012-12-09 05:25:46 -0500

    switch command line arg parser

--- nodeless.js
+++ nodeless.js
@@ -3,12 +3,15 @@
 var fs = require('fs');
 var less = require('less');
 var path = require('path');
-var program = require('commander');
 
-program
-	.version('0.0.1')
-	.option('-x, --compress', 'Compress output by removing some whitespaces.')
-	.parse(process.argv);
+var argv = require('optimist')
+	.usage('Usage: $0 [-x] [-o dir] files')
+	.boolean('x')
+	.alias('x', 'compress')
+	.describe('x', 'Compress output by removing some whitespaces.')
+	.alias('o', 'output')
+	.describe('o', 'Output all compiled CSS to the specified directory.')
+	.argv;
 
 function compile(file) {
 	var source = path.relative(process.cwd(), file);
@@ -44,14 +47,37 @@ function search(filename) {
 		}
 		else if(stats.isFile() && path.extname(filename) == '.less') {
 			var toWatch = path.relative(process.cwd(), filename);
-			console.log('Adding %s to watch list.', toWatch);
-			fs.watch(toWatch, function(event) {
-				console.log('%s modified', toWatch);
-				//compile(toWatch);
-			});
+			//console.log('Adding %s to watch list.', toWatch);
+			watch(toWatch);
 		}
 	});
 }
 
-search(process.cwd());
+function watch(filename) {
+	fs.watch(filename, function(event) {
+		compile(filename);
+	});
+	console.log('Watching %s', path.relative(process.cwd(), filename));
+}
+
+if(!argv._.length) {
+	search(process.cwd());
+} else {
+	for(var i = 0; i < argv._.length; i++)
+	{
+		var watched = path.resolve(argv._[i]);
+		fs.stat(watched, function(err, stats) {
+			if(err)
+				return console.log('Error describing %s: %s', watched, err);
+
+			if(stats.isDirectory()) {
+				search(watched);
+			} else if(stats.isFile()) {
+				watch(watched);
+			}
+		});
+	}
+}
+
+//search(process.cwd());
 //compile('style.less');
